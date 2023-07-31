b120d9ec53341004887743badee80d1852e66bf7
Vinicius Santos <viniciusantos0898@gmail.com>
2023-07-31 20:26:04 -0300

    Added the presentation; added viewBinding

--- app/src/main/java/br/com/entrypoint/mycinemalistyt/presentation/MainActivity.kt
+++ app/src/main/java/br/com/entrypoint/mycinemalistyt/presentation/MainActivity.kt
@@ -2,11 +2,39 @@ package br.com.entrypoint.mycinemalistyt.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.viewbinding.ViewBinding
 import br.com.entrypoint.mycinemalistyt.R
+import br.com.entrypoint.mycinemalistyt.databinding.ActivityMainBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val mainViewModel: MainViewModel by viewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.call.setOnClickListener {
+            callPopularMovies()
+        }
+
+        mainViewModel.popularMovies.observe(this,
+        ){
+            popularMovies -> popularMovies.forEach {
+                Log.i("movies", it.title)
+            }
+        }
+    }
+
+    private fun callPopularMovies(){
+        GlobalScope.launch{
+            mainViewModel.getPopularMovies()
+        }
     }
 }
